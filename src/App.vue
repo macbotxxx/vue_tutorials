b361937c4e7a091e2e6400699fd074f92f66cf9f
Michael Asa <assanamamichael@gmail.com>
2023-09-25 17:44:07 +0100

    vue counter using js

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+let count = 0;
+
+const addToCount = () => {
+  count += 1;
+}
+
+const subtractCount = () => {
+  count -= 1;
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div>
+      <h1>Counter</h1>
+      <h1>{{ count  }}</h1>
+      <button > - </button>
+      <button > + </button>
+  
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style scope>
+
+main {
+  background-color: bisque;
+  width: 100vw;
+  height: 100vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+div {
+  text-align: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
